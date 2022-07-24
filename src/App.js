cad0b99d3dfd2d0231e68e0ab0ff1c6749aa5693
taeyun <ew20@naver.com>
2022-07-24 22:42:04 +0900

    NETFLIX : NavBar 컴포넌트 생성하기

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
-// const API_KEY = "91ee4b65589660d9a2368fe66cb604cf";
+import Nav from "./components/Nav";
 
-// APU 요청 예) => https://api.themoviedb.org/3/movie/550?api_key=91ee4b65589660d9a2368fe66cb604cf
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
     </div>
   );
 }
--- src/api/axios.js
+++ src/api/axios.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+// APU 요청 예) => https://api.themoviedb.org/3/movie/550?api_key=91ee4b65589660d9a2368fe66cb604cf
 
 // axios인스턴스화 하기
 const instance = axios.create({
